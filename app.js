929c3fc2c0c9925badd97b5e2578662d1eaa9cd7
Sai Teja Jammalamadaka <tsjamm@gmail.com>
2013-05-02 04:10:23 +0530

    port to process variable

--- app.js
+++ app.js
@@ -2,7 +2,16 @@ require('./server/js/main');
 
 var connect = require('connect');
 var app = connect();
-var port = 8080;
+
+var ipaddress = process.env.OPENSHIFT_INTERNAL_IP;
+var port      = process.env.OPENSHIFT_INTERNAL_PORT || 8080;
+
+        if (typeof ipaddress === "undefined") {
+            //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
+            //  allows us to run/test the app locally.
+            console.warn('No OPENSHIFT_INTERNAL_IP var, using 127.0.0.1');
+            ipaddress = "127.0.0.1";
+
 
 // Display incoming requests, coloured for status
 app.use(connect.logger('dev'));
