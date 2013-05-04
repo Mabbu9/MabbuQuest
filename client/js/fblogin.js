7c3bdb2e27377c1f43cb17c86338906aad071523
Nitin Sreeram <s.nani100@gmail.com>
2013-05-04 18:05:28 +0530

    fb login

--- client/js/fblogin.js
+++ client/js/fblogin.js
@@ -1,14 +1,7 @@
-<html>
-<head>
-
-</head>
-<body>
-<div id="fb-root"></div>
-<script>
-  window.fbAsyncInit = function() {
+ window.fbAsyncInit = function() {
   FB.init({
     appId      : '275215129268204', // App ID
-    //channelUrl : '/channel.html', // Channel File
+    channelUrl : 'http://cli-mabbuquest.rhcloud.com/channel.html', // Channel File
     status     : true, // check login status
     cookie     : true, // enable cookies to allow the server to access the session
     xfbml      : true  // parse XFBML
@@ -46,3 +39,22 @@
   });
   testAPI();
   };
+
+  // Load the SDK asynchronously
+  (function(d){
+   var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement('script'); js.id = id; js.async = true;
+   js.src = "http://connect.facebook.net/en_US/all.js";
+   ref.parentNode.insertBefore(js, ref);
+  }(document));
+
+  // Here we run a very simple test of the Graph API after login is successful. 
+  // This testAPI() function is only called in those cases. 
+  function testAPI() {
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+      console.log('Good to see you, ' + response.name + '.');
+	  $('#nameinput').val(response.name);
+    });
+  }
